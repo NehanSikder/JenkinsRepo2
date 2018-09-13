9d81b85d2a83af8728f65fda13cdfb7cb3537c1a
Nehan <ans2ur@virginia.edu>
2018-09-12 23:33:53 -0400

    first commit

---
+++ src/test/java/PassTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+
+public class PassTest {
+
+        @Test
+        public void this_should_pass(){
+            assertTrue(true);
+        }
+
+        @Test
+        public void this_should_pass_too(){
+            assertTrue(true);
+        }
+
+}
